469d4110e360b5163407fff314e0037fd34c8c47
l <l>
2022-07-17 11:52:26 +0000

    - update

--- main.c
+++ main.c
@@ -9,16 +9,15 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-int main(int argc, char **argv) {
-  // Check argument provided
-  if (argc != 2) {
-    fprintf(stderr, "[-] Usage: %s INPUT_FILE\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
+struct elf_handle {
+  char *elf_file;
+  size_t file_size;
+};
 
+struct elf_handle read_elf_file(const char *filename) {
   // Open file to read/wrtite
-  printf("[+] Opening %s\n", argv[1]);
-  FILE *fd = fopen(argv[1], "rw+");
+  printf("[+] Opening %s\n", filename);
+  FILE *fd = fopen(filename, "rw+");
   if (fd == NULL) {
     perror("fopen");
     exit(EXIT_FAILURE);
@@ -26,7 +25,7 @@ int main(int argc, char **argv) {
 
   // Get file size
   struct stat file_stat;
-  stat(argv[1], &file_stat);
+  stat(filename, &file_stat);
   size_t file_size = file_stat.st_size;
   printf("[+] File is %lu bytes\n", file_size);
 
@@ -38,9 +37,27 @@ int main(int argc, char **argv) {
     perror("mmap");
     exit(EXIT_FAILURE);
   }
-  Elf64_Ehdr *elf_header = (Elf64_Ehdr *)elf_file;
-  Elf64_Shdr *shdr = (Elf64_Shdr *)(elf_file + elf_header->e_shoff);
-  char *strhdr_table = elf_file + shdr[elf_header->e_shstrndx].sh_offset;
+  fclose(fd);
+
+  // Build output struct
+  struct elf_handle eh;
+  eh.elf_file = elf_file;
+  eh.file_size = file_size;
+  return eh;
+}
+
+int main(int argc, char **argv) {
+  // Check argument provided
+  if (argc != 2) {
+    fprintf(stderr, "[-] Usage: %s INPUT_FILE\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // Build view of elf file internals
+  struct elf_handle eh = read_elf_file(argv[1]);
+  Elf64_Ehdr *elf_header = (Elf64_Ehdr *)eh.elf_file;
+  Elf64_Shdr *shdr = (Elf64_Shdr *)(eh.elf_file + elf_header->e_shoff);
+  char *strhdr_table = eh.elf_file + shdr[elf_header->e_shstrndx].sh_offset;
 
   // Strip sections
   for (int i = 1; i < elf_header->e_shnum; ++i) {
@@ -53,8 +70,7 @@ int main(int argc, char **argv) {
   elf_header->e_shoff = 0;
 
   printf("[+] Saving file back to disk and cleaning up\n");
-  msync(elf_file, file_size, MS_SYNC);
-  munmap(elf_file, file_size);
-  fclose(fd);
+  msync(eh.elf_file, eh.file_size, MS_SYNC);
+  munmap(eh.elf_file, eh.file_size);
   return EXIT_SUCCESS;
 }
